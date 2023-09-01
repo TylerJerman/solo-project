48e7b3281cc72e65692100a481bc7e5736c29b70
Tyler Jerman <Tyler.s.jerman@gmail.com>
2023-09-01 09:27:39 -0600

    ALL DONE FOR REAL

--- server.js
+++ server.js
@@ -17,15 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
-// Logging middleware for debugging
-// app.use((req, res, next) => {
-//   console.log(`Incoming request: ${req.method} ${req.path}`);
-//   next(); 
-// });
 ViteExpress.config({printViteDevServerHost: true});
 
 //routes go here
-  //this will fetch all products from my database
 app.get('/api/products', async (req, res) => {
   try {
     const products = await Item.findAll();
